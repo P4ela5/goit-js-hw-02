9508129daab987102d9054ed3ce877fe167dff45
Lesia <157366599+P4ela5@users.noreply.github.com>
2024-03-12 16:22:56 +0200

    Updated HW

---
+++ js/task-4.js
@@ -0,0 +1,28 @@
+function getShippingCost(country) {
+    const China = 100;
+    const Chile = 250;
+    const Australia = 170;
+    const Jamaica = 120;
+    let message;
+    country=country.toLowerCase();
+    switch(country) {
+        case "china": message = `Shipping to China will cost ${China} credits`;
+        break;
+        case "chile": message = `Shipping to Chile will cost ${Chile} credits`;
+        break;
+        case "australia": message = `Shipping to Australia will cost ${Australia} credits`;
+        break;
+        case "jamaica": message = `Shipping to Jamaica will cost ${Jamaica} credits`;
+        break;
+        default: message= "Sorry, there is no delivery to your country";
+    }
+
+    return message;
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
